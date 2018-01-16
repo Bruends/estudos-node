e81f367e086fc1750de3d7dbe369cb3400b1c6a2
Bruno Mendes <brunomendes962@gmail.com>
2018-01-16 13:11:10 -0200

    api testavel aulas de 1 a 3

---
+++ api-testavel/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+app.route('/books')
+  .get((req, res) => {
+    res.json([
+      {
+        id: 1,
+        name: 'Default Book'
+      }
+    ])
+  })
+  
+module.exports = app;
